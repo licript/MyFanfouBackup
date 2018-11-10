2b287421552add92f65f0cbb1b54ad5f783db3c7
liyang <liyang@billionss.com>
2018-11-10 14:46:44 +0800

    init proj

---
+++ main.py
@@ -0,0 +1,64 @@
+# _*_  coding: utf-8 _*_
+import requests
+from lxml import html
+from bs4 import BeautifulSoup
+
+login_url = "http://fanfou.com/login"
+url = 'http://fanfou.com/login?fr=%2Flogin'
+url_home = 'http://fanfou.com/lovelemontea'
+
+
+
+headers_get = {
+    'Host': 'fanfou.com',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'DNT': '1',
+    'Referer': 'http://fanfou.com/',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6'
+    }
+
+headers_login = {
+    'Host': 'fanfou.com',
+    'Connection': 'keep-alive',
+    'Content-Length': '163',
+    'Cache-Control': 'max-age=0',
+    'Origin': 'http://fanfou.com',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'DNT': '1',
+    'Referer': 'http://fanfou.com/',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6'
+    }
+
+session_requests = requests.session()
+
+resp = session_requests.get(login_url, headers=headers_get)
+
+# url_from = BeautifulSoup(resp.content, "lxml").find('input', attrs={'name':'urlfrom'})['value']
+payload ={
+    "loginname": "li2.y@foxmail.com",
+    "loginpass": "li19930427",
+    "token": "5b5c2b15",
+    "action": "login",
+    # 'urlfrom': url_from,
+}
+
+res = session_requests.post(url, data=payload, headers=headers_login)
+
+# print(res, '\nres')
+
+def handle_page(s, url):
+    res_home = s.get(url)
+    soup = BeautifulSoup(res_home.content, "lxml")
+    contents = soup.findAll('span', class_='content')
+    for content in contents:
+        print(content)
+
+handle_page(session_requests, url_home)
